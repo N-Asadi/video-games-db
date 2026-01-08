a3e60bc5c39f5be848a313c1ff2e53ec68b16346
Nasratullah Asadi <166419406+N-Asadi@users.noreply.github.com>
2026-01-08 15:28:56 -0700

    Update Footer.js
    Footer was assigned as a client component.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,10 +1,12 @@
+"use client";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-8 mt-16">
       <div className="container mx-auto text-center">
         <p className="mb-4">
-          &copy; {new Date().getFullYear()} GameVerse. All Rights Reserved.
-        </p>
+      &copy; {new Date().getFullYear()} GameVerse. All Rights Reserved.
+    </p>
         <p className="text-sm text-gray-400">
           Powered by IGDB API. GameVerse is not affiliated with or endorsed by
           IGDB.
